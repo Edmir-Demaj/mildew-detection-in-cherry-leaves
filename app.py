c56b18911743c7f34514618368e48e9513bbf99d
Edmir-Demaj <>
2023-09-17 14:41:32 +0000

    Add mildew detection page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
+#load pages scripts
 from app_pages.multipage import MultiPage
 from app_pages.project_overview_page import project_overview_page
 from app_pages.usage_guide_page import usage_guide_page
 from app_pages.project_hypothesis import project_hypothesis_page
 from app_pages.leaf_visualizer_page import leaf_visualizer_page
+from app_pages.powdery_mildew_detection_page import powdery_mildew_detection_page
 
 app = MultiPage(app_name="Mildew Detection in Cherry Leaves")
 app.add_page("Project Overview", project_overview_page)
